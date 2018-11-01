e3e3e93d0010eefd51c78ccb2519dd208b10055e
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-11-01 09:09:46 +0000

    Created tables and migration files; form for figures/new.erb and get '/figures/new' in figures_controller.rb

---
+++ db/migrate/02_create_tables_figures_and_titles_and_figure_titles.rb
@@ -0,0 +1,16 @@
+class CreateTablesFiguresAndTitlesAndFigureTitles < ActiveRecord::Migration
+  def change
+    create_table :figures do |t|
+      t.string :name
+    end
+
+    create_table :titles do |t|
+      t.string :name
+    end
+
+    create_table :figure_titles do |t|
+      t.integer :title_id
+      t.integer :figure_id
+    end
+  end
+end
